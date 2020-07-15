a84b6e37e943fdc9625bf56189675672027fd1e1
sakshi9401 <68016527+sakshi9401@users.noreply.github.com>
2020-07-15 19:17:51 +0530

    Add files via upload

---
+++ list in python.py
@@ -0,0 +1,22 @@
+#list
+number = [5, 7, 8, 3, 4, 7, 9, 0]
+number.append(10)
+print(number)
+number.reverse()
+print(number)
+number.sort()
+print(number)
+tp = (1, 3, 5, 6)
+print(tp)
+tp = (4)
+print(tp)
+tp = (1,)
+print(tp)
+#swapping
+n = 5
+m = 9
+#temp = n
+#n = m
+#m = temp
+(n ,m) = (m , n)
+print(n , m)
